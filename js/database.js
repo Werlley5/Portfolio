76ad1299c2436dbf04e42c5909a90715e49a53b8
Werlley5 <110196227+Werlley5@users.noreply.github.com>
2023-05-10 22:17:32 -0300

    Add files via upload

---
+++ js/database.js
@@ -0,0 +1,110 @@
+const projetos = [
+    {
+        img: "./img/site-games.PNG",
+        name: "Loja de Games",
+        skills: "html - css - js",
+        link1: "https://werlley5.github.io/loja-de-games/",
+        link2: "https://github.com/Werlley5/loja-de-games.git",
+        button1: "ver projeto",
+        button2: "ver código"
+    },
+    {
+        img: "./img/site-institucional.PNG",
+        name: "Site Institucional",
+        skills: "html - css - js",
+        link1: "https://werlley5.github.io/site-institucional/",
+        link2: "https://github.com/Werlley5/site-institucional.git",
+        button1: "ver projeto",
+        button2: "ver código"
+    },
+    {
+        img: "./img/intro.PNG",
+        name: "Intro section",
+        skills: "html - css - js",
+        link1: "https://werlley5.github.io/HTML-e-CSS-9/",
+        link2: "https://github.com/Werlley5/HTML-e-CSS-9.git",
+        button1: "ver projeto",
+        button2: "ver código"
+    },
+    {
+        img: "./img/site-noticias.PNG",
+        name: "Site de Notícias",
+        skills: "html - css - js",
+        link1: "https://werlley5.github.io/HTML-e-CSS-7/",
+        link2: "https://github.com/Werlley5/HTML-e-CSS-7.git",
+        button1: "ver projeto",
+        button2: "ver código"
+    },
+    {
+        img: "./img/site-topografia.PNG",
+        name: "Site de Topografia",
+        skills: "html - css",
+        link1: "https://werlley5.github.io/site-de-topografia/",
+        link2: "https://github.com/Werlley5/site-de-topografia.git",
+        button1: "ver projeto",
+        button2: "ver código"
+    },
+    {
+        img: "./img/accordion.PNG",
+        name: "Accordion Card",
+        skills: "html - css - js",
+        link1: "https://werlley5.github.io/HTML-e-CSS-8/",
+        link2: "https://github.com/Werlley5/HTML-e-CSS-8.git",
+        button1: "ver projeto",
+        button2: "ver código"
+    },
+    {
+        img: "./img/huddle.PNG",
+        name: "Huddle",
+        skills: "html - css",
+        link1: "https://werlley5.github.io/HTML-e-CSS-6/",
+        link2: "https://github.com/Werlley5/HTML-e-CSS-6.git",
+        button1: "ver projeto",
+        button2: "ver código"
+    },
+    {
+        img: "./img/e-commerce.PNG",
+        name: "E-Commerce",
+        skills: "html - css",
+        link1: "https://werlley5.github.io/HTML-e-CSS-5/",
+        link2: "https://github.com/Werlley5/HTML-e-CSS-5.git",
+        button1: "ver projeto",
+        button2: "ver código"
+    },
+    {
+        img: "./img/nft.PNG",
+        name: "NFT",
+        skills: "html - css",
+        link1: "https://werlley5.github.io/HTML-e-CSS-4/",
+        link2: "https://github.com/Werlley5/HTML-e-CSS-4.git",
+        button1: "ver projeto",
+        button2: "ver código"
+    },
+    {
+        img: "./img/3-column.PNG",
+        name: "3-Column",
+        skills: "html - css",
+        link1: "https://werlley5.github.io/HTMLe-CSS-3/",
+        link2: "https://github.com/Werlley5/HTMLe-CSS-3.git",
+        button1: "ver projeto",
+        button2: "ver código"
+    },
+    {
+        img: "./img/parfum.PNG",
+        name: "Parfum",
+        skills: "html - css",
+        link1: "https://werlley5.github.io/HTML-e-CSS-1/",
+        link2: "https://github.com/Werlley5/HTML-e-CSS-1.git",
+        button1: "ver projeto",
+        button2: "ver código"
+    },
+    {
+        img: "./img/qr-code.PNG",
+        name: "QR-code",
+        skills: "html - css",
+        link1: "https://werlley5.github.io/HTML-e-CSS-2/",
+        link2: "https://github.com/Werlley5/HTML-e-CSS-2.git",
+        button1: "ver projeto",
+        button2: "ver código"
+    },
+]
