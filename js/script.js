49ed97cdbc3c9023eae52c2de6a207e2cda93eb3
Werlley <110196227+Werlley5@users.noreply.github.com>
2023-07-21 16:14:14 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const cont = document.querySelector('.mobile-menu');
-const sidebar = document.querySelector('.hamburger');
-
-sidebar.addEventListener('click', () => {
-
-    cont.classList.toggle('show-menu');
-    
-});
